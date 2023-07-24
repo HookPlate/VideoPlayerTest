792283b9be11bf78835556c6e640a57011db1256
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-07-24 19:17:57 +0100

    this url works

--- VideoPlayerTest/ContentView.swift
+++ VideoPlayerTest/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
     var body: some View {
         VideoPlayer(player: AVPlayer(url: url))
             .onAppear {
-                
+                player.play()
             }
             
             
